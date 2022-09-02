53f4cd29f7255f3e877879ce2b58abdddf721a1b
sourabh Jain <sourabhjain0412@gmail.com>
2022-09-02 14:17:19 +0530

    second batch uploading

---
+++ src/com/first/Hello.java
@@ -0,0 +1,14 @@
+package com.first;
+
+public class Hello {
+	public static void main(String args[])
+	{
+		/*this is comment
+		 sdfsfsdfsdffdfdsf
+		
+		 */
+		char b ='s'+'o'+'u'+'r'+'a'+'b'+'h';
+		System.out.println("Hello"+" "+ b);
+	}
+}
+
